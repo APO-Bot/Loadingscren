804489d4a7d1e2649f36462433fd6dc58b22c8b9
APO <59201044+APO-Bot@users.noreply.github.com>
2021-05-13 12:37:42 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -113,7 +113,7 @@ var l_musicDisplay = true;
 // (Place the ogg files in the music folder)
 // (You can also use external URL for the ogg files)
 var l_musicPlaylist = [
-	{youtube: "https://www.youtube.com/watch?v=iuJDhFRDx9M", name: "Tokyo Drift"},
+	{},
 	
 ];
 
